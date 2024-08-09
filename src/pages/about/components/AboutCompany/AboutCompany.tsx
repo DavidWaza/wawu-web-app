4e28cd2ee4b6430547caf327d93662f83015bb84
david waza <davidyega69@gmail.com>
2024-08-09 17:48:14 +0100

    chore: changed welcome text

--- src/pages/about/components/AboutCompany/AboutCompany.tsx
+++ src/pages/about/components/AboutCompany/AboutCompany.tsx
@@ -6,17 +6,18 @@ const AboutCompany = () => {
         <Heading variant="medium" fontColor="secondary">
           Welcome to Wawu Technology Home of the Best Girl Tech Spearheads
         </Heading>
-        <Text variant="small" className="text-black 2xl:w-full mt-3 leading-[25px] md:leading-[30px]">
-          By nature, we are incubators, creators, caretakers, protectors &
-          preservers. We are everything that’s required to serve enterprises
-          through cutting edge digital solutions & experiences. <br />Stewards that
-          observe ongoing trends and analyze upcoming technologies to transform
-          the digital persona of clients with our unbeatable performance. <br />Wawu
-          Technology believes in a world where access to knowledge, innovation
-          and business opportunities are based on interest and ability, not
-          gender. <br />We do everything to make women feel included, seen and
-          compensated no matter where we call home, so they can live decently,
-          live their dreams and support their families back home.
+        <Text
+          variant="small"
+          className="text-black 2xl:w-full mt-3 leading-[25px] md:leading-[30px] sora"
+        >
+          Wawu Africa from the word (Wow) is an offspring of Kudimata is working
+          to become the world&apos;s work marketplace for women professionals,
+          artisans and the Best Girl Tech Spearheads connecting businesses and
+          talent to unlock their potential. By nature, women are stewards,
+          incubators, creators, caretakers, Protectors & Preservers. Our mission
+          is to create economic opportunities for women to have brighter
+          futures. As a result, we are working to be the marketplace where any
+          woman from around the globe meets to accomplish incredible things.
         </Text>
       </div>
     </>
